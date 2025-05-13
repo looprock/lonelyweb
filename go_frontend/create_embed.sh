0100a0a580e8c1b745b33088398c06eb17d9c0c4
Douglas Land <dsl@pop-os.localdomain>
2025-05-12 19:50:00 -0500

    Fixed a typo

--- go_frontend/create_embed.sh
+++ go_frontend/create_embed.sh
@@ -4,5 +4,6 @@ tmp_db="youtube_collector/data/tmp.db"
 cd /home/dsl/git/uncommitted/lonelyweb
 rm -rf ${tmp_db}
 cp ${source_db} ${tmp_db}
+cp ${source_db} ~/Dropbox/lonelyweb/
 sqlite3 ${tmp_db} "delete from videos where view_count > 99;"
 sqlite3 ${tmp_db} .dump > go_frontend/data/embedded_data.sql
